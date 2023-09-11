312698b6c04e3dac8a3639d6629d21a6e43ea023
Juhyoung Park <joo98@github.com>
2023-09-11 13:18:36 +0900

    init baseline template

---
+++ arg_parser.py
@@ -0,0 +1,16 @@
+#from parser_script.iris_classification import init_parser
+from arg_parser import init_parser
+from training import training
+#from preprocessing import Preprocessing
+from testing import testing
+
+# Commit message template test용 @
+
+if __name__=='__main__':
+    args = init_parser()
+
+    if args.training:
+        training(args)
+
+    if args.testing:
+        testing(args)
